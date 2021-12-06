54c4b09600d726e1530548130e9bfc12f950b044
bogdzn <wontsleep@protonmail.com>
2021-12-06 16:46:39 +0100

    fix -- formatting

--- priv/repo/migrations/20211206154241_create_addresses.exs
+++ priv/repo/migrations/20211206154241_create_addresses.exs
@@ -3,12 +3,12 @@ defmodule Naboo.Repo.Migrations.CreateAddresses do
 
   def change do
     create table(:addresses) do
-      add :city, :string
-      add :state_district, :string
-      add :state, :string
-      add :postcode, :string
-      add :country, :string
-      add :country_code, :string
+      add(:city, :string)
+      add(:state_district, :string)
+      add(:state, :string)
+      add(:postcode, :string)
+      add(:country, :string)
+      add(:country_code, :string)
 
       timestamps()
     end
