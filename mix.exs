466b9c449e072b12e4159f0e7394e7930793ca45
bogdzn <wontsleep@protonmail.com>
2021-12-07 23:17:01 +0100

    fixing test rule by running seeds with it

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Naboo.Mixfile do
       ],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.reset", "test"]
     ]
   end
 
