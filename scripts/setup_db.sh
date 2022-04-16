77939351871f47a13423b30b246df9cdd0e392ba
bogdzn <wontsleep@protonmail.com>
2022-04-16 21:24:21 +0200

    (fix/scripts)postgres setup on dev and env

--- scripts/setup_db.sh
+++ scripts/setup_db.sh
@@ -1,12 +1,21 @@
 #!/bin/env bash
 
+envup () {
+    if [ -f ${1} ]; then
+        set -o allexport
+        export $(grep -v '#.*' ${1} | xargs)
+        set +o allexport
+    else echo "Could not find ${1}"
+    fi
+}
+
 install_postgresql () {
     echo "installing postgresql"
 
      export POSTGRES_USER="${USER}"
 	 export POSTGRES_PASSWORD="postgres"
-	 export POSTGRES_DB="naboo"
-     export POSTGRES_TEST_DB="${POSTGRES_DB}_test"
+	 export POSTGRES_DB="naboo_dev"
+     export POSTGRES_TEST_DB="naboo_test"
 
 	 sudo apt-get install -y postgresql postgresql-contrib libpq-dev || true
 	 sudo -u postgres psql -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
@@ -17,4 +26,13 @@ install_postgresql () {
 	 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_TEST_DB TO $POSTGRES_USER;"
 }
 
-install_postgresql && mix ecto.create
+ecto_create () {
+    MIX_ENV=$1
+
+    envup .env.${MIX_ENV} && mix ecto.create
+}
+
+install_postgresql || exit 1
+
+ecto_create "dev"
+ecto_create "test"
