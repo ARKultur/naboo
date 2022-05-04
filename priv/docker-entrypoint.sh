8118334a0cd3a76e3d2c03941e91b590f2812830
bogdzn <wontsleep@protonmail.com>
2022-05-04 14:51:57 +0200

    (fix/build)entrypoint.sh would not be recognised by Dockerfile

--- priv/docker-entrypoint.sh
+++ priv/docker-entrypoint.sh
@@ -5,9 +5,9 @@ set -euo pipefail
 # check if postgresql-client is installed
 which pg_isready >/dev/null || { echo "Postgres is not installed. Exiting." && exit 1 ; }
 
-#while ! pg_isready -q -h "${POSTGRES_HOST}" -p "${POSTGRES_PORT}" -U "${POSTGRES_USER}"; do
-#    echo "$(date) - waiting for database to start"
+while ! pg_isready -q -h "${POSTGRES_HOST}"; do
+    echo "$(date) - waiting for database to start"
     sleep 1
-#done
+done
 
 mix ecto.setup && exec mix phx.server
