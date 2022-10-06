db31860d873ddd7dafe4b2e2b6f93b68e0c56d0c
winds0r <wontsleep@protonmail.com>
2022-10-06 10:47:43 +0900

    (mod/config)bumping version+using fetch_env! instead of get_env
    Release configuration guide:
    https://hexdocs.pm/mix/1.14/Mix.Tasks.Release.html#module-vm-args-and-env-sh-env-bat

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Naboo.Mixfile do
   def project do
     [
       app: :naboo,
-      version: "0.3.2",
+      version: "0.4.0",
       elixir: "~> 1.14",
       erlang: "~> 24.1",
       elixirc_paths: elixirc_paths(Mix.env()),
