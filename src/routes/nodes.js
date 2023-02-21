f07b4f8ef0c0dbf699a6cf89833331d4cb332542
SheItoon <guillaume.debard@epitech.eu>
2023-02-22 02:59:01 +0900

    Add nodes documentation

--- src/routes/nodes.js
+++ src/routes/nodes.js
@@ -5,6 +5,172 @@ import {Node, User, Organisation} from '../db/models/index.js'
 
 let node_router = express.Router()
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Node:
+ *       type: object
+ *       required:
+ *         - name
+ *         - longitude
+ *         - latitude
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: The auto-generated id of the node
+ *         name:
+ *           type: string
+ *           description: The node's name
+ *         addressId:
+ *           type: integer
+ *           description: The referenced address's id
+ *         OrganisationId:
+ *           type: integer
+ *           description: The referenced organisation's id
+ *         longitude:
+ *           type: number
+ *           description: The longitude of the node
+ *         latitude:
+ *           type: number
+ *           description: The latitude of the node
+ *       example:
+ *         id: 12
+ *         name: Louvre
+ *         addressId: 5
+ *         OrganisationId: 1
+ *         longitude: 152
+ *         latitude: 35
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Nodes
+ *   description: The Nodes managing API
+ * /api/nodes:
+ *   post:
+ *     summary: Create a Node
+ *     tags: [Nodes]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *            required:
+ *             - name
+ *             - longitude
+ *             - latitude 
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *              longitude:
+ *                type: number
+ *              latitude:
+ *                type: number
+ *     responses:
+ *       200:
+ *         description: The created Node
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Node'
+ *       500:
+ *         description: Node already existing
+ *   patch:
+ *     summary: Edit a Node
+ *     tags: [Nodes]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *            required:
+ *             - name 
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *              longitude:
+ *                type: number
+ *              latitude:
+ *                type: number
+ *              address:
+ *                type: integer
+ *              organisation:
+ *                type: integer
+ *     responses:
+ *       200:
+ *         description: The edited Node
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Node'
+ *       404:
+ *         description: Node not found
+ * 
+ *   delete:
+ *     summary: Delete a Node
+ *     tags: [Nodes]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *            required:
+ *             - name 
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *     responses:
+ *       200:
+ *         description: Confirmation string
+ *         content:
+ *           application/json:
+ *             schema:
+ *              type: object
+ *              properties:
+ *                body:
+ *                  type: string
+ *       404:
+ *         description: Node not found
+ *   get:
+ *     summary: Lists all the Nodes
+ *     tags: [Nodes]
+ *     responses:
+ *       200:
+ *         description: The list of the Nodes
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Node'
+ * 
+ * /api/nodes/{names}:
+ *   get:
+ *     summary: Get Node by name
+ *     tags: [Nodes]
+ *     parameters:
+ *       - in: path
+ *         name: name
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The Node's name
+ *     responses:
+ *       200:
+ *        description: The Node response by name
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Node'
+ *       404:
+ *         description: Node not found
+ */
+
 node_router.get('/', authenticateTokenAdm, async (req, res) => {
     const nodes = await Node.findAll();
     res.send(nodes.toJSON())
@@ -59,7 +225,7 @@ node_router.post('/', authenticateToken, async (req, res) => {
         return res.send(node.toJSON())
     } catch (error) {
         console.log(err);
-        res.status(401).send("node already existing");
+        res.status(500).send("node already existing");
     }
 })
 
@@ -92,6 +258,7 @@ node_router.delete('/', authenticateToken, async (req, res) => {
       if (node)
       {
         node.delete()
+        res.send("success")
       } else {
         res.status(404).send("Node not found");
       }
