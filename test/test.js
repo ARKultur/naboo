9835cccd683f27987c3b0bf8f6078a1ff5b1d6ab
Shelton <guillaume.debard@epitech.eu>
2023-03-13 18:18:05 +0900

    WIP unit test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+import {expect} from 'chai'
+import request from 'supertest'
+
+describe('test route', function () {
+    let app
+    before('Mock db connection and load app', async function () {
+        app = require('..')
+    })
+})
+
