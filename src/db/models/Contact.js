547666f83325b4c6e046821bfd5c900383e57254
Guillaume S <guillaume.soisson.69@gmail.com>
2023-03-13 16:35:08 +0900

    Add contact form

---
+++ src/db/models/Contact.js
@@ -0,0 +1,45 @@
+import {  DataTypes, Model } from 'sequelize';
+
+export default class Contact extends Model {
+
+  static definition(sequelize) {
+    Contact.init(
+      {
+        id: {
+          type: DataTypes.UUID,
+          allowNull: false,
+          primaryKey: true,
+          defaultValue: DataTypes.UUIDV4
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        category: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        description: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        processed: {
+          type: DataTypes.BOOLEAN,
+          allowNull: false,
+          defaultValue: false,
+        }
+      },
+      {
+        sequelize: sequelize,
+        tableName: 'contact',
+      }
+    )
+  }
+
+  static associate() {
+  }
+}
