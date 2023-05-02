ee7078ab3c7dc576480b1460541a2554e98e3a98
SheItoon <56004854+SheItoon@users.noreply.github.com>
2023-05-02 11:37:58 +0900

    Test webhook

--- src/routes/documentation.js
+++ src/routes/documentation.js
@@ -8,7 +8,7 @@ const options = {
     definition: {
         openapi: "3.0.0",
         info: {
-          title: "ARKultur backend API with Swagger",
+          title: "ARKultur backend API with Swaggerrrr",
           version: "0.1.0",
           description:
             "This is a CRUD API made with Express and documented with Swagger",
