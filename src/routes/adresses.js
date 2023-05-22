0ad72a6b8d86ccb1e5177edc06ac8194dc42788c
Shelton <guillaume.debard@epitech.eu>
2023-05-22 20:30:38 +0900

    update address

--- src/routes/adresses.js
+++ src/routes/adresses.js
@@ -4,6 +4,42 @@ import {authenticateTokenAdm, authenticateToken} from "../utils.js";
 
 const adress_router = express.Router();
 
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      Address:
+ *        type: object
+ *        required:
+ *          - country
+ *          - postcode
+ *          - state
+ *          - street_address
+ *          - city
+ *        properties:
+ *          country:
+ *            type: string
+ *            description: The country where the address is located.
+ *          postcode:
+ *            type: number
+ *            description: The postal code of the address.
+ *          state:
+ *            type: string
+ *            description: The state where the address is located.
+ *          street_address:
+ *            type: string
+ *            description: The street name and number of the address.
+ *          city:
+ *            type: string
+ *            description: The city where the address is located.
+ *        example:
+ *           country: USA
+ *           postcode: 90210
+ *           state: California
+ *           street_address: 123 Beverly Hills
+ *           city: Los Angeles
+ */
+
 adress_router.get('/admin', authenticateTokenAdm, async (req, res) => {
     const addresses = await Adress.findAll();
     res.send(addresses);
@@ -63,11 +99,7 @@ adress_router.patch('/', authenticateToken, async (req, res) => {
 		postcode: postcode || addr.postcode,
 		state: state || addr.state,
 		city: city || addr.city,
-		street_address: street_address || addr.street_address,
-		CustomerId: CustomerId || addr.CustomerId,
-		NodeId: NodeId || addr.NodeId,
-		OrganisationId: OrganisationId || addr.OrganisationId,
-		UserId: UserId || addr.UserId
+		street_address: street_address || addr.street_address
 	    });
 	    res.send(addr)
 	} else
