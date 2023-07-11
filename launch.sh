67b0aa75c775aa9d358d7bae92bcf620b3f1cf1f
SheItoon <56004854+SheItoon@users.noreply.github.com>
2023-07-12 04:53:11 +0900

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -6,14 +6,11 @@ if [ "$NPM_COMMAND" == "test" ]; then
     docker-compose up --build -d db --exit-code-from app
     sleep 5
     docker-compose up --build app
-    EXIT_CODE=$(docker wait app)
+    EXIT_CODE=$(docker wait naboo_app_1)
     docker-compose down
     exit "$EXIT_CODE"
 else
     docker-compose up --build -d db
     sleep 5
     docker-compose up --build app
-    EXIT_CODE=$(docker wait app)
-    docker-compose down
-    exit "$EXIT_CODE"
 fi
