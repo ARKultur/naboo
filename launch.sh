6baff17b9547ad05690cd9fcaad7bb6fb5602a96
SheItoon <56004854+SheItoon@users.noreply.github.com>
2023-07-12 04:30:37 +0900

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -5,6 +5,7 @@ source .env
 if [ "$NPM_COMMAND" == "test" ]; then
     docker-compose up --build -d db --exit-code-from app
     sleep 5
+    docker-compose up --build app
     EXIT_CODE=$(docker wait app)
     docker-compose down
     exit "$EXIT_CODE"
